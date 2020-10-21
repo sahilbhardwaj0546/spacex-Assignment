53bc81606bb018075ff1b84b4cc54eb9b6539061
sahil.bhardwaj11@outlook.com <sahilbhardwaj0546@gmail.com>
2020-10-21 18:41:56 +0530

    heroku_fix

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-//Install express server
 const express = require('express');
 const path = require('path');
 
@@ -6,11 +5,7 @@ const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/publicis'));
-
 app.get('/*', function(req,res) {
-    
 res.sendFile(path.join(__dirname+'/dist/publicis/index.html'));
 });
-
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
