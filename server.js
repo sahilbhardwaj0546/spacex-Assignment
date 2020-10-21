4e958da3d5d42b40ec2c2703c168508d83cbeb4a
sahil.bhardwaj11@outlook.com <sahilbhardwaj0546@gmail.com>
2020-10-22 00:06:16 +0530

    e

--- server.js
+++ server.js
@@ -1,27 +1,12 @@
 const express = require('express');
-const path = require('path');
-const app = express();
-app.use(express.static(__dirname + '/dist/publicis'));
-app.get('/*', function(req,res) {
-res.sendFile(path.join(__dirname+'/dist/publicis/index.html'));
-});
-app.listen(process.env.PORT || 8080);
-
-
-const express = require('express');
 const http = require('http')
 const path = require('path');
-
 const app = express();
-path.join(__dirname, "path/to/index") 
-app.use(express.static(path.join(__dirname, 'dist/publicis')));
-
+app.use(path.join(__dirname, "path/to/index"));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/publicis/index.html'));
 });
-
 const port = process.env.PORT || 3000;
 app.set('port', port);
-
 const server = http.createServer(app);
 server.listen(port, () => console.log('running'));
